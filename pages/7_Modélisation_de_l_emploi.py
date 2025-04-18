c6b8ce8b43efd60af30019428ee4692a5388ce4c
amanseur <amina.manseur@ensae.fr>
2025-04-18 14:55:55 +0000

    Ajout de logging

--- pages/7_Modélisation_de_l_emploi.py
+++ pages/7_Modélisation_de_l_emploi.py
@@ -3,14 +3,20 @@ Ce module génère la page Modélisation de l'emploi de l'application de visuali
 """
 
 import streamlit as st
+from loguru import logger
 from src.models_visualisation_utils import (
     get_data_log_regression,
     get_model_performance,
 )
 
-# ==========================
-# User interface
-# ==========================
+# Initialisation du logger
+logger.add(
+    "logs/modelisation_emploi.log",
+    rotation="1 MB",
+    retention="10 days",
+    level="DEBUG",
+)
+logger.info("Début de la page Streamlit : Modélisation de l'emploi")
 
 # Configuration de la page
 st.set_page_config(
@@ -41,12 +47,23 @@ st.markdown(
 VAR_NUM = ["PreviousSalary", "YearsCode", "YearsCodePro", "ComputerSkills"]
 VAR_CAT = ["Age", "EdLevel", "Gender", "MentalHealth", "MainBranch"]
 
+# Log de la sélection des variables
+logger.info("Demande de sélection des variables pour la régression logistique.")
 list_col = tab_logistic_regression.multiselect(
     "Sélectionner les variables explicatives pour la régression de la variable 'Employed' : ",
     VAR_NUM + VAR_CAT,
     default=VAR_NUM[1:] + VAR_CAT,
 )
+
+# Log des paramètres sélectionnés
+logger.debug(f"Variables sélectionnées : {list_col}")
+
+# Obtenir les données et les résultats de la régression logistique
 result_df, score, X, delta_prob = get_data_log_regression(parameters=list_col)
+
+# Log de la performance du modèle
+logger.info(f"Modèle de régression logistique : R2 = {round(score * 100, 2)}%")
+
 tab_logistic_regression.subheader(f"Le R2 du modèle est : {round(score * 100, 2)}%")
 tab_logistic_regression.table(result_df)
 
@@ -56,12 +73,17 @@ tab_logistic_regression.table(result_df)
 
 tab_models_performance.subheader("Random Forest performance")
 result_df_exp2 = get_model_performance("Random Forest")
+logger.info("Affichage des résultats de performance pour le modèle Random Forest.")
 tab_models_performance.table(result_df_exp2)
 
 tab_models_performance.subheader("Logistic Regression performance")
 result_df_exp3 = get_model_performance("Logistic Regression")
+logger.info("Affichage des résultats de performance pour le modèle Logistic Regression.")
 tab_models_performance.table(result_df_exp3)
 
 tab_models_performance.subheader("Gradient Boosting performance")
 result_df_exp4 = get_model_performance("Gradient Boosting")
+logger.info("Affichage des résultats de performance pour le modèle Gradient Boosting.")
 tab_models_performance.table(result_df_exp4)
+
+logger.info("Fin de l'exécution de la page Modélisation de l'emploi")
